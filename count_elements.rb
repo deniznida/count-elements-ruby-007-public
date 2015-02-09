d5e748f084a23f965843558730882fe37943d440
deniznida <denizunat@hotmail.com>
2015-02-09 09:13:12 -0500

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  #code goes here
+  #count = {}
+  #array.each do |element|
+  #  count = counts[element] += 1
+  #end
+Hash[array.group_by{|i| i}.map{|k,v| [k,v.size]}]
+
 end
  
